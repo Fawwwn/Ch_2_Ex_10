78b4616bcfe63d70ddef2ee928908be46c88636a
isaiah pryor <starlingisaiah@gmail.com>
2024-10-25 07:21:34 -0400

    Add project files.

---
+++ Ch_2_Ex_10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	using std::cout;
+
+
+	const int GAL = 15;
+
+	const int MILE = 375;
+
+	const int MPG = MILE / GAL;
+
+	cout << "This is the MPG you'll receive: " << MPG;
+	
+
+
+	return 0;
+}
